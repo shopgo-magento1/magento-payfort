077e8743ea86dc69a2bda057a66ad7c56a616326
msaleh@payfort.com <msaleh@payfort.com>
2015-11-01 14:16:27 +0200

    fix show host to host

--- app/code/community/Payfort/Pay/Block/Note.php
+++ app/code/community/Payfort/Pay/Block/Note.php
@@ -7,6 +7,10 @@ class Payfort_Pay_Block_Note extends Mage_Adminhtml_Block_System_Config_Form_Fie
 		switch($element_id):
 			case 'more_info':
 				return '<a href="http://www.payfort.com/contact-us/" target="_blank">Contact Us</a>';
+                break;
+                case 'host_to_host_url':
+                return Mage::getBaseUrl() . 'payfort/payment/response/';
+                break;
 			break;
 			// case 'how_to_test':
 				// return '<a href="https://secure.payfort.com/Ncol/PayFort_Testacc_EN.pdf?CSRFSP=%2fncol%2ftest%2fbackoffice%2fsupportgetdownloaddocument.asp&CSRFKEY=83E267BD93379EC7A63B9D5BDBE67B83E81240E9&CSRFTS=20130906221442&branding=PAYFORT" target="_blank">How to create a test account</a>';
